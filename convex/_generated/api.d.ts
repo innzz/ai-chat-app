e0e2a4f00533903160d672cb954b82d9e3a88c6e
inzamam <inzamam.pawaskar10@gmail.com>
2024-05-02 16:29:42 +0500

    Webhooks added

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as clerk from "../clerk.js";
 import type * as http from "../http.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,7 +27,9 @@ import type * as http from "../http.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  clerk: typeof clerk;
   http: typeof http;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
