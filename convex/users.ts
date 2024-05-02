55c66166f21b32578528f6c295620a3649b30385
inzamam <inzamam.pawaskar10@gmail.com>
2024-05-02 17:47:17 +0500

    Create conversation functionality added

--- convex/users.ts
+++ convex/users.ts
@@ -78,7 +78,8 @@ export const getUsers = query({
 		}
 
 		const users = await ctx.db.query("users").collect();
-		return users.filter((user) => user.tokenIdentifier !== identity.tokenIdentifier);
+		// return users.filter((user) => user.tokenIdentifier !== identity.tokenIdentifier);
+		return users;
 	},
 });
 
